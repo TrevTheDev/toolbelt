07ce3d59afd32a6906b239f1776b27e71dcc6974
TrevTheDev <trevor.rabie@gmail.com>
2021-12-21 07:02:31 +0000

    npm edits

--- src/createUid.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var createUid = function (length) {
-    if (length === void 0) { length = 20; }
-    return Array.from({ length: length }, function () { return Math.random().toString(36)[2]; }).join('');
-};
-exports.default = createUid;
